843f4bdb3f6a57517903b5d42aec8ea02c246582
Jimena <jimena@Mac-mini-de-Jimena.local>
2024-01-06 18:48:47 -0300

    design navbar

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -5,6 +5,7 @@ import MyOrder from '../MyOrder'
 import MyOrders from '../MyOrders'
 import NotFound from '../NotFound'
 import SignIn from '../SignIn'
+import Navbar from '../../components/Navbar'
 import './App.css'
 
 
@@ -29,6 +30,7 @@ const App = ()  =>{
   return (
    <BrowserRouter >
     <AppRoutes />
+    <Navbar />
    </BrowserRouter>
 
 
