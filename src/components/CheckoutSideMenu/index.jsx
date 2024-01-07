75a74d7fb9cf0c71ce16a7012ace1b4a41a7c848
Jimena <jimena@Mac-mini-de-Jimena.local>
2024-01-07 14:03:59 -0300

    evitar duplicidad al agregar al carrito

--- src/components/CheckoutSideMenu/index.jsx
+++ src/components/CheckoutSideMenu/index.jsx
@@ -23,7 +23,7 @@ const CheckoutSideMenu = () =>{
                     className="h-6 w-6 text-black cursor-pointer" />
                 </div>
             </div>
-            <div className='px-6'>
+            <div className='px-6 overflow-y-scroll'>
                     {
                         context.cartProducts.map( (product) => (<OrderCard 
                                                                     key={product.id}
